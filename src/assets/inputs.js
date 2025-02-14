0eff05762bd15a5019439a66d1ea79789589f7d2
R3ndrex <yacholohchtoli@gmail.com>
2025-02-14 23:21:42 +0200

    Create file for inputs data

---
+++ src/assets/inputs.js
@@ -0,0 +1,23 @@
+import PhoneImage from "../assets/phone.svg";
+import MailImage from "../assets/email.svg";
+import HomeImage from "../assets/home.svg";
+const inputs = [
+    {
+        image: PhoneImage,
+        alt: "Phone image",
+        placeholder: "Enter your phone number",
+        id: 0,
+    },
+    {
+        image: MailImage,
+        placeholder: "Enter your email address",
+        id: 1,
+    },
+    {
+        image: HomeImage,
+        placeholder: "Enter your address",
+        id: 2,
+    },
+];
+
+export default inputs;
