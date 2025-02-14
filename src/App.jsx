254a1ca9fe2ae41a0d5c564d91c75f657f3ff9f9
R3ndrex <yacholohchtoli@gmail.com>
2025-02-14 23:22:25 +0200

    Import sections, use states for changable sections,import style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Contact from "./components/ContactSection.jsx";
+import ContactChanger from "./components/ContactChanger.jsx";
+import { useState } from "react";
+import AboutChanger from "./components/AboutChanger.jsx";
+import About from "./components/About.jsx";
+import Education from "./components/Education.jsx";
+import WorkExperience from "./components/WorkExperience.jsx";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [contacts, setContacts] = useState([]);
+    const [education, setEducation] = useState([]);
+    const [aboutText, setAboutText] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>CV-Generator</h1>
+            </header>
+            <aside className="main-aside">
+                <ContactChanger contacts={contacts} setContacts={setContacts} />
+                <AboutChanger setAboutText={setAboutText} />
+            </aside>
+            <main>
+                <About>{aboutText}</About>
+                <Education>{education}</Education>
+                <aside className="cv-aside">
+                    <Contact contacts={contacts} />
+                    <WorkExperience />
+                </aside>
+            </main>
+        </>
+    );
 }
-
-export default App
