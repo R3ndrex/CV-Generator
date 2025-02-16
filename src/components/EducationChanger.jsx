ffedcc2977ebf896aa320de7e7dab82b714ec042
R3ndrex <yacholohchtoli@gmail.com>
2025-02-16 16:00:57 +0200

    Add education input from array, make focus on one that is used, remove empty element on blur

--- src/components/EducationChanger.jsx
+++ src/components/EducationChanger.jsx
@@ -1,9 +1,39 @@
-export default function EducationChanger({ education }) {
+import { useState } from "react";
+export default function EducationChanger({ education, setEducation }) {
+    const [focus, setFocus] = useState(null);
     return (
         <>
             <section>
                 <h2>Education </h2>
-                <ul></ul>
+                <ul>
+                    {education.map((edu, index) => {
+                        return (
+                            <input
+                                key={index}
+                                type="text"
+                                value={edu}
+                                onFocus={() => setFocus(index)}
+                                autoFocus={focus === index}
+                                onBlur={() =>
+                                    education.includes("") &&
+                                    education.length > 1 &&
+                                    setEducation((prev) =>
+                                        prev.filter(
+                                            (element) => element.trim() !== ""
+                                        )
+                                    )
+                                }
+                                onChange={(e) => {
+                                    setEducation((prev) => {
+                                        const changed = [...prev];
+                                        changed[index] = e.target.value;
+                                        return changed;
+                                    });
+                                }}
+                            ></input>
+                        );
+                    })}
+                </ul>
             </section>
         </>
     );
