be2f9ce91659ec395803ed18e088d288fd28a1af
R3ndrex <yacholohchtoli@gmail.com>
2025-02-17 23:45:39 +0200

    Add animation classes to chevron

--- src/components/ChangerHeader.jsx
+++ src/components/ChangerHeader.jsx
@@ -6,15 +6,13 @@ export default function ChangerHeader({ children }) {
         <>
             <div
                 className="h2-section"
-                onClick={() => {
-                    setActive((prev) => !prev);
-                }}
+                onClick={() => setActive((prev) => !prev)}
             >
                 {children[0]}
                 {children[1]}
                 <img
                     src={Chevron}
-                    className="h2-pictogram"
+                    className={`h2-pictogram ${active && "rotated"}`}
                     alt="chevron-down"
                 />
             </div>
