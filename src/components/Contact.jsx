8796cd62cd7a2f71865a9ed85b1414bfe95bc34e
R3ndrex <yacholohchtoli@gmail.com>
2025-02-20 14:22:35 +0200

    Remove section if empty contacts array

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,33 @@
 import { inputs } from "../assets/inputs";
 
 export default function Contact({ contacts }) {
+    function CheckEmpty(array) {
+        return array.filter((element) => element !== "") <= 0;
+    }
     return (
         <>
-            <section>
-                <h2>Contact</h2>
-                <ul>
-                    {contacts.map((contact, index) => {
-                        return (
-                            <li key={inputs[index].id}>
-                                {contact !== "" && contact !== undefined && (
-                                    <img
-                                        className="pictogram"
-                                        src={inputs[index].image}
-                                        alt={inputs[index].alt}
-                                    />
-                                )}
-                                {contact}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </section>
+            {!CheckEmpty(contacts) && (
+                <section>
+                    <h2>Contact</h2>
+                    <ul>
+                        {contacts.map((contact, index) => {
+                            return (
+                                <li key={inputs[index].id}>
+                                    {contact !== "" &&
+                                        contact !== undefined && (
+                                            <img
+                                                className="pictogram"
+                                                src={inputs[index].image}
+                                                alt={inputs[index].alt}
+                                            />
+                                        )}
+                                    {contact}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </section>
+            )}
         </>
     );
 }
