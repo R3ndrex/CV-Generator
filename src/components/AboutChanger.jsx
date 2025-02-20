79b774a29e356f164f1d3d0e50f2de6b1563c891
R3ndrex <yacholohchtoli@gmail.com>
2025-02-20 15:08:23 +0200

    Make function to handle submit

--- src/components/AboutChanger.jsx
+++ src/components/AboutChanger.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import Account from "../assets/account.svg";
 export default function AboutChanger({ aboutText, Accordion, setAboutText }) {
     const [defaultAboutText, setDefaultAboutText] = useState(aboutText);
+    function handleSubmit(e) {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        setAboutText(form.get("aboutText"));
+        setDefaultAboutText(form.get("aboutText"));
+    }
     return (
         <section>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    const form = new FormData(e.currentTarget);
-                    setAboutText(form.get("aboutText"));
-                    setDefaultAboutText(form.get("aboutText"));
-                }}
-            >
+            <form onSubmit={handleSubmit}>
                 <Accordion
                     image={{ src: Account, alt: "person-image" }}
                     title="About"
