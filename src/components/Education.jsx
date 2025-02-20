1cb9f92f4547839dd4d805963ad219f409c15d20
R3ndrex <yacholohchtoli@gmail.com>
2025-02-20 15:13:16 +0200

    Remove section if empty education array

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,28 +1,35 @@
 import "../styles/education.css";
 export default function Education({ children }) {
     return (
-        <section>
-            <h2>Education</h2>
-            <ul className="education">
-                {children.map((element) => {
-                    return (
-                        element !== "" &&
-                        element !== undefined && (
-                            <li className="educationLi" key={element.id}>
-                                <div>{element.schoolName}</div>
-                                <div>
-                                    {element.dateStart}
-                                    {element.dateStart &&
-                                        element.dateEnd &&
-                                        "-"}
-                                    {element.dateEnd}
-                                </div>
-                                <div>{element.title}</div>
-                            </li>
-                        )
-                    );
-                })}
-            </ul>
-        </section>
+        <>
+            {children.length > 0 && (
+                <section>
+                    <h2>Education</h2>
+                    <ul className="education">
+                        {children.map((element) => {
+                            return (
+                                element !== "" &&
+                                element !== undefined && (
+                                    <li
+                                        className="educationLi"
+                                        key={element.id}
+                                    >
+                                        <div>{element.schoolName}</div>
+                                        <div>
+                                            {element.dateStart}
+                                            {element.dateStart &&
+                                                element.dateEnd &&
+                                                "-"}
+                                            {element.dateEnd}
+                                        </div>
+                                        <div>{element.title}</div>
+                                    </li>
+                                )
+                            );
+                        })}
+                    </ul>
+                </section>
+            )}
+        </>
     );
 }
