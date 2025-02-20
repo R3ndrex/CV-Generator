2cea06b7024e58c32fe8c3b17e6306995437c002
R3ndrex <yacholohchtoli@gmail.com>
2025-02-20 14:15:22 +0200

    Remove section if work is empty array

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,23 +1,27 @@
 export default function WorkExperience({ children }) {
     return (
-        <section>
-            <h2>Work experience</h2>
-            <ul>
-                {children.map((work) => (
-                    <li className="work-item" key={work.id}>
-                        <div>{work.companyName}</div>
-                        <div className="date-container">
-                            {work.dateStart && work.dateEnd && (
-                                <>
-                                    ({work.dateStart})-({work.dateEnd})
-                                </>
-                            )}
-                        </div>
-                        <div>{work.title}</div>
-                        <p>{work.respons}</p>
-                    </li>
-                ))}
-            </ul>
-        </section>
+        <>
+            {children.length !== 0 && (
+                <section>
+                    <h2>Work experience</h2>
+                    <ul>
+                        {children.map((work) => (
+                            <li className="work-item" key={work.id}>
+                                <div>{work.companyName}</div>
+                                <div className="date-container">
+                                    {work.dateStart && work.dateEnd && (
+                                        <>
+                                            ({work.dateStart})-({work.dateEnd})
+                                        </>
+                                    )}
+                                </div>
+                                <div>{work.title}</div>
+                                <p>{work.respons}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            )}
+        </>
     );
 }
