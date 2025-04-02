4dba583fbc2037b28048f82f332e38fde2d8ab27
R3ndrex <yacholohchtoli@gmail.com>
2025-04-02 23:08:20 +0300

    Remove image if no image specified

--- src/components/CV/UserImage.jsx
+++ src/components/CV/UserImage.jsx
@@ -1,15 +1,5 @@
-import AccountCircle from "../../assets/account-circle.svg";
-
 export default function UserImage({ src }) {
     return (
-        <img
-            className="user-photo"
-            src={
-                src === null || src === undefined || src === ""
-                    ? AccountCircle
-                    : src
-            }
-            alt="user photo"
-        />
+        <>{src && <img className="user-photo" src={src} alt="user photo" />}</>
     );
 }
