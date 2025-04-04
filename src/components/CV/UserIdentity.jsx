19cdfe27819923463bef53a7dd6eee6a24bb254e
R3ndrex <yacholohchtoli@gmail.com>
2025-04-04 13:25:44 +0300

    Remove section if no data specified

--- src/components/CV/UserIdentity.jsx
+++ src/components/CV/UserIdentity.jsx
@@ -1,10 +1,19 @@
 export default function UserIdentity({ generalInfo }) {
     return (
-        <div>
-            {generalInfo.fullName && (
-                <h2 className="margin-bottom-0">{generalInfo.fullName}</h2>
-            )}
-            {generalInfo.profession && <h3>{generalInfo.profession}</h3>}
-        </div>
+        <>
+            {generalInfo.fullName !== "" ||
+                (generalInfo.profession !== "" && (
+                    <section>
+                        {generalInfo.fullName && (
+                            <h2 className="margin-bottom-0">
+                                {generalInfo.fullName}
+                            </h2>
+                        )}
+                        {generalInfo.profession && (
+                            <h3>{generalInfo.profession}</h3>
+                        )}
+                    </section>
+                ))}
+        </>
     );
 }
