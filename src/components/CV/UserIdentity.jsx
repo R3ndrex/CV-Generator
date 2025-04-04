8b4db70268b29171e6a4f99700edfddbfe4624e7
R3ndrex <yacholohchtoli@gmail.com>
2025-04-04 15:03:29 +0300

    Fix not showing useridentity at all

--- src/components/CV/UserIdentity.jsx
+++ src/components/CV/UserIdentity.jsx
@@ -1,19 +1,18 @@
 export default function UserIdentity({ generalInfo }) {
     return (
         <>
-            {generalInfo.fullName !== "" ||
-                (generalInfo.profession !== "" && (
-                    <section>
-                        {generalInfo.fullName && (
-                            <h2 className="margin-bottom-0">
-                                {generalInfo.fullName}
-                            </h2>
-                        )}
-                        {generalInfo.profession && (
-                            <h3>{generalInfo.profession}</h3>
-                        )}
-                    </section>
-                ))}
+            {(generalInfo.fullName !== "" || generalInfo.profession !== "") && (
+                <section>
+                    {generalInfo.fullName && (
+                        <h2 className="margin-bottom-0">
+                            {generalInfo.fullName}
+                        </h2>
+                    )}
+                    {generalInfo.profession && (
+                        <h3>{generalInfo.profession}</h3>
+                    )}
+                </section>
+            )}
         </>
     );
 }
