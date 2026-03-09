7b2f6fe33531e38827b2cd08829f72858bea6723
R3ndrex <yacholohchtoli@gmail.com>
2026-03-09 13:50:58 +0200

    remove accordion prop and import

--- src/components/CVChanger/CvChanger.tsx
+++ src/components/CVChanger/CvChanger.tsx
@@ -2,7 +2,6 @@ import ContactChanger from "./ContactChanger";
 import AboutChanger from "./AboutChanger";
 import EducationChanger from "./EducationChanger";
 import GeneralInfoChanger from "./GeneralInfoChanger";
-import Accordion from "./Accordion";
 import WorkExperienceChanger from "./WorkExperienceChanger";
 import SkillsChanger from "./SkillsChanger";
 import ProjectsChanger from "./ProjectsChanger";
@@ -12,13 +11,13 @@ export default function CvChanger({ showEdit }: { showEdit: boolean }) {
         <aside
             className={`main-aside ${(!showEdit && "disabled") || "enabled"}`}
         >
-            <GeneralInfoChanger Accordion={Accordion} />
-            <ContactChanger Accordion={Accordion} />
-            <AboutChanger Accordion={Accordion} />
-            <EducationChanger Accordion={Accordion} />
-            <WorkExperienceChanger Accordion={Accordion} />
-            <SkillsChanger Accordion={Accordion} />
-            <ProjectsChanger Accordion={Accordion} />
+            <GeneralInfoChanger />
+            <ContactChanger />
+            <AboutChanger />
+            <EducationChanger />
+            <WorkExperienceChanger />
+            <SkillsChanger />
+            <ProjectsChanger />
         </aside>
     );
 }
